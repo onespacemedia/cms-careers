3299facdf352a9c589409ac4730d67714c57c73d
Daniel Samuels <daniel.samuels1@gmail.com>
2015-11-15 20:48:09 +0000

    Switch to the new URLs.py style. Ref onespacemedia/cms#80.

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -1,10 +1,9 @@
 """ URLs used by the jobs app """
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import JobListView, JobDetailView
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(r"^$", JobListView.as_view(), name='job_list'),
     url(r"^(?P<url_title>[a-z-]+)/$", JobDetailView.as_view(), name='job'),
-)
+]
