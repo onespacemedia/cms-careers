db50c01298fd7887e13f665b17966ca8c9145302
Daniel Samuels <daniel.samuels1@gmail.com>
2017-01-25 13:56:32 +0000

    Fix URLs
    The previous version didn't allow slugs with numbers, even though they're valid. This one is more permissive.

--- apps/careers/urls.py
+++ apps/careers/urls.py
@@ -4,5 +4,5 @@ from .views import CareerDetailView, CareerListView
 
 urlpatterns = [
     url(r'^$', CareerListView.as_view(), name='career_list'),
-    url(r'^(?P<slug>[a-z-]+)/$', CareerDetailView.as_view(), name='career'),
+    url(r'^(?P<slug>[^/]+)/$', CareerDetailView.as_view(), name='career'),
 ]
